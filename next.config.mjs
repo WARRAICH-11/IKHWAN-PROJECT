a130c49213baf31badddc541bba674075e6baca6
HASSAN WARRAICH <hm2257153@gmail.com>
2026-05-07 06:03:03 +0500

    nxtjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextconfig = {
+  basePath: '/IKHWAN-PROJECT', // Set the base path for your project
   output: 'export', // Adds static export
   images: {
     unoptimized: true, // Required for static export
